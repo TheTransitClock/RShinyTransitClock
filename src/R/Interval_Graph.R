7bf05f1f93d5b38f8586a1256203862b1b7da2ab
Simon Berrebi <32271736+simonberrebi@users.noreply.github.com>
2020-02-15 19:31:10 -0500

    Three Dashboards
    Ten_Min_Error_By_Route: 
    I believe this is a newer version. The version in src>R>PredictionErrorByRoute has dbSendQuery + Fetch whereas the version I am posting has dbGetQuery, which doesn't require Fetch. Also my version is from November 5th.
    - Note that this code contains two figures 1) all route mean and st.dev of error and 2) prediction error ranked for all routes.
    
    Interval Graph: 
    Also from November 5th. I assume you already have the latest version

---
+++ src/R/Interval_Graph.R
@@ -0,0 +1,97 @@
+
+install.packages("RMySQL")
+install.packages("ggplot2")
+install.packages("ddply")
+install.packages("data.table")
+library(RMySQL)
+library(ggplot2)
+library(ddply)
+library(plyr)
+library(data.table)
+
+# ----- LOAD AND PROCESS MT DATA FROM PLAYBACK
+
+#PlayBack
+mydb_PB = dbConnect(MySQL(), user='transitime', password='transitimemn', dbname='transitime_mnrt', host='127.0.0.1', port = 33309)
+
+fetch_db_PB = dbGetQuery(mydb_PB, "
+SELECT
+time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) as Horizon,
+time_to_sec(timediff(arrivalDepartureTime,predictedTime)) as Error,
+predictionSource
+#, PredictionAccuracy.* 
+  FROM transitime_mnrt.PredictionAccuracy
+where time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) mod 30 = 0
+and time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) BETWEEN 29 AND 1201
+and arrivalDepartureTime BETWEEN '2019-10-28' AND '2019-11-01' 
+and predictionSource = 'GTFS-rt'
+ORDER BY Horizon, predictionSource, error
+limit 2000000;
+", n=1000000)
+
+Data_MT_Quantile <- do.call("rbind", tapply(fetch_db_PB$Error, fetch_db_PB$Horizon, quantile, c(0.05, 0.125, 0.5, 0.875, 0.95)))
+MT_Pred <- setDT(as.data.frame(Data_MT_Quantile), keep.rownames = TRUE)[]
+colnames(MT_Pred) <- c('Horizon', 'P_05', 'P_15', 'P_50', 'P_85', 'P_95')
+MT_Pred$Horizon <- as.numeric(MT_Pred$Horizon)
+
+# ----- LOAD AND PROCESS TTC DATA FROM DEV
+
+# Dev
+mydb_Dev = dbConnect(MySQL(), user='transitime', password='transitimemn', dbname='transitime_mnrt', host='127.0.0.1', port = 33312)
+
+fetch_db_Dev = dbGetQuery(mydb_Dev, "
+SELECT
+time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) as Horizon,
+time_to_sec(timediff(arrivalDepartureTime,predictedTime)) as Error,
+predictionSource
+#, PredictionAccuracy.* 
+  FROM transitime_mnrt.PredictionAccuracy
+where time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) mod 30 = 0
+and time_to_sec(timediff(arrivalDepartureTime,predictionReadTime)) BETWEEN 29 AND 1201
+and arrivalDepartureTime BETWEEN '2019-10-28' AND '2019-11-01' 
+and predictionSource = 'TransitClock'
+ORDER BY Horizon, predictionSource, error
+limit 2000000;
+", n=1000000)
+
+Data_TTC_Quantile <- do.call("rbind", tapply(fetch_db_Dev$Error, fetch_db_Dev$Horizon, quantile, c(0.05, 0.125, 0.5, 0.875, 0.95)))
+TTC_Pred <- setDT(as.data.frame(Data_TTC_Quantile), keep.rownames = TRUE)[]
+colnames(TTC_Pred) <- c('Horizon', 'P_05', 'P_15', 'P_50', 'P_85', 'P_95')
+TTC_Pred$Horizon <- as.numeric(TTC_Pred$Horizon)
+
+# ----- PLOT
+
+ggsave(filename = "Interval_Oct28_31.png", path = '/Users/simonberrebi2/Dropbox/KF_Paper/Figures/Intervals',
+ggplot() +
+  geom_line(data = MT_Pred, aes(y=P_05, x=Horizon, color="NexTrip"), size=0) +
+  geom_line(data = MT_Pred, aes(y=P_15, x=Horizon, color="NexTrip"), size=0)+
+  geom_line(data = MT_Pred, aes(y=P_50, x=Horizon, color="NexTrip"), size=0.5)+
+  geom_line(data = MT_Pred, aes(y=P_85, x=Horizon, color="NexTrip"), size=0)+
+  geom_line(data = MT_Pred, aes(y=P_95, x=Horizon, color="NexTrip"), size=0)+
+  geom_ribbon(data = MT_Pred, aes(ymin=P_05,ymax=P_15, x=Horizon, fill="NexTrip", alpha="90 Percent"))+
+  geom_ribbon(data = MT_Pred, aes(ymin=P_15,ymax=P_50, x=Horizon, fill="NexTrip", alpha="75 Percent"))+
+  geom_ribbon(data = MT_Pred, aes(ymin=P_50,ymax=P_85, x=Horizon, fill="NexTrip", alpha="75 Percent"))+
+  geom_ribbon(data = MT_Pred, aes(ymin=P_85,ymax=P_95, x=Horizon, fill="NexTrip", alpha="90 Percent")) +
+  geom_line(data = TTC_Pred, aes(y=P_05, x=Horizon, color="TheTransitClock"), size=0) +
+  geom_line(data = TTC_Pred, aes(y=P_15, x=Horizon, color="TheTransitClock"), size=0)+
+  geom_line(data = TTC_Pred, aes(y=P_50, x=Horizon, color="TheTransitClock"), size=0.5)+
+  geom_line(data = TTC_Pred, aes(y=P_85, x=Horizon, color="TheTransitClock"), size=0)+
+  geom_line(data = TTC_Pred, aes(y=P_95, x=Horizon, color="TheTransitClock"), size=0)+
+  geom_ribbon(data = TTC_Pred, aes(ymin=P_05,ymax=P_15, x=Horizon, fill="TheTransitClock", alpha="90 Percent"))+
+  geom_ribbon(data = TTC_Pred, aes(ymin=P_15,ymax=P_50, x=Horizon, fill="TheTransitClock", alpha="75 Percent"))+
+  geom_ribbon(data = TTC_Pred, aes(ymin=P_50,ymax=P_85, x=Horizon, fill="TheTransitClock", alpha="75 Percent"))+
+  geom_ribbon(data = TTC_Pred, aes(ymin=P_85,ymax=P_95, x=Horizon, fill="TheTransitClock", alpha="90 Percent")) +
+  theme_bw() +
+  theme(axis.title = element_text(face = "bold"), legend.position="bottom",plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) + 
+  ggtitle("Prediction Error Quantiles Against Horizon 10/28-11/1 2019") +
+  ylab("Prediction Error (Actual-Predicted) in Seconds") + xlab("Horizon (Actual-Prediction Time) in Seconds")+
+  scale_x_continuous(expand = c(0, 00), limits = c(0,1200))+
+  #scale_fill_manual(name = "", values = c("NexTrip" = "red", "TheTransitClock" = "blue")) +
+  scale_alpha_manual(name = "", values = c("75 Percent" = 0.4, "90 Percent" = 0.2)) +
+  #scale_color_manual(name = "", values = c("NexTrip" = "red", "TheTransitClock" = "blue"))+
+  #scale_size_manual(name = "", values = c("Median" = 0.5))+
+   labs(fill = "Quantile:") + labs(color = " Median:"),
+width = 10, height = 5)
+
+
+
---
+++ src/R/Ten_Min_Error_By_Route.R
@@ -0,0 +1,95 @@
+
+install.packages("RMySQL")
+install.packages("ggplot2")
+library(RMySQL)
+library(ggplot2)
+
+# ----- LOAD AND PROCESS MT DATA FROM PLAYBACK
+
+mydb_PB = dbConnect(MySQL(), user='transitime', password='transitimemn', dbname='transitime_mnrt', host='127.0.0.1', port = 33309)
+
+Ten_Min_By_Route_PB = dbGetQuery(mydb_PB, "select 
+routeshortname, predictionsource, count(*) as sample_size,
+avg(time_to_sec(timediff(arrivalDepartureTime,predictedTime))) as mean_error, 
+stddev(time_to_sec(timediff(arrivalDepartureTime,predictedTime))) as std_error  
+FROM transitime_mnrt.PredictionAccuracy
+where arrivalDepartureTime BETWEEN '2019-10-28' AND '2019-11-01'
+and predictionsource = 'GTFS-rt'
+and time_to_sec(timediff(arrivalDepartureTime,predictionReadTime))  BETWEEN 590 AND 610
+group by predictionsource, routeshortname",n=-1)
+
+
+# ----- LOAD AND PROCESS TTC DATA FROM DEV
+mydb_Dev = dbConnect(MySQL(), user='transitime', password='transitimemn', dbname='transitime_mnrt', host='127.0.0.1', port = 33312)
+
+Ten_Min_By_Route_Dev = dbGetQuery(mydb_Dev, "select 
+routeshortname, predictionsource, count(*) as sample_size,
+avg(time_to_sec(timediff(arrivalDepartureTime,predictedTime))) as mean_error, 
+stddev(time_to_sec(timediff(arrivalDepartureTime,predictedTime))) as std_error  
+FROM transitime_mnrt.PredictionAccuracy
+where arrivalDepartureTime BETWEEN '2019-10-28' AND '2019-11-01'
+and predictionsource = 'TransitClock'
+and time_to_sec(timediff(arrivalDepartureTime,predictionReadTime))  BETWEEN 590 AND 610
+group by routeshortname",n=-1)
+
+# ----- COMBINING 
+
+Ten_Min_By_Route_PB$predictionsource <- 'NexTrip'
+Ten_Min_By_Route_Dev$predictionsource <- 'TheTransitClock'
+Ten_Min_By_Route <- rbind(Ten_Min_By_Route_PB, Ten_Min_By_Route_Dev)
+
+
+# One Option consists in summarizing everything first: https://rcompanion.org/handbook/C_04.html
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+#                               Comparison of Std. Error on 
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+Cum_Ten_Min_By_Route <- Ten_Min_By_Route
+Cum_Ten_Min_By_Route$routeshortname <- factor(Cum_Ten_Min_By_Route$routeshortname, levels = Cum_Ten_Min_By_Route$routeshortname[order(subset(Cum_Ten_Min_By_Route, predictionsource == 'NexTrip')$std_error)])
+Cum_Ten_Min_By_Route <- Cum_Ten_Min_By_Route[order(Cum_Ten_Min_By_Route$routeshortname),]
+Cum_Ten_Min_By_Route$routeshortname <- rep(1:(nrow(Cum_Ten_Min_By_Route)/2), each=2)
+  
+
+
+ggsave(filename = "Cum_Ten_Min_By_Route_Oct_28_31.png", path = '/Users/simonberrebi2/Dropbox/KF_Paper/Figures/Cumulative_Error',
+ggplot(subset(Cum_Ten_Min_By_Route, sample_size >50),                ### The data frame to use.
+       aes(x     = routeshortname,
+           y     = std_error,
+           color = predictionsource)) +
+  geom_point(shape = 15,
+             size  = 2) +
+  theme_bw() +
+  theme(axis.title = element_text(face = "bold"), legend.position="bottom") + ggtitle("Prediction Error Comparison By Route on 600 Second Horizon") +
+  ylab("Prediction Std. Error in Seconds")+ xlab("Number of Routes")+ labs(color = "Prediction Source:")+
+  scale_x_continuous(expand = c(0, 00), limits = c(0,180)), width = 10, height = 4)
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+#                               Comparison of Std. Error on 
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Ten_Min_By_Route$routeshortname <- factor(Ten_Min_By_Route$routeshortname, levels = unique(as.numeric(Ten_Min_By_Route$routeshortname)))
+pd = position_dodge(.4)    ### How much to jitter the points on the plot
+
+ggsave(filename = "Ten_Min_By_Route_Oct_28_31_Express2.png", path = '/Users/simonberrebi2/Dropbox/KF_Paper/Figures/Distribution_By_Route',
+ggplot(subset(Ten_Min_By_Route, sample_size > 50 & as.numeric(routeshortname) >  650),                ### The data frame to use.
+       aes(x     = routeshortname,
+           y     = mean_error,
+           color = predictionsource)) +
+  geom_point(shape = 15,
+             size  = 2,
+             position = pd) +
+   geom_errorbar(aes(ymin  = mean_error - std_error,
+                    ymax  =  mean_error + std_error),
+               width = 0.2,
+              size  = 1.0,
+              #size = sample_size/100,
+             position = pd) +
+  theme_bw() +
+  theme(axis.title = element_text(face = "bold"), legend.position="bottom") + ggtitle("Prediction Distribution Comparison By Route on 600 Second Horizon") +
+  ylab("Prediction Distribution (Mean ± Std. Error) in Seconds") + xlab("Route Number")+ labs(color = "Prediction Source:"), width = 15, height = 5)
+
+#scale_color_manual(name = "", values = c("GTFS-rt" = "red", "TransitClock" = "blue"))
+
+
+
